80069a664e3937113bb4ecdd8742a07dd42d847b
unknown <dmitro.horev@gmail.com>
2018-06-08 22:05:09 +0300

    first commit

---
+++ StringFilter/Filter.cs
@@ -0,0 +1,33 @@
+﻿namespace StringFilter
+{
+    public struct Filter
+    {
+        public string FindFirstUniqChar(string str)
+        {
+            var counter = new int[26];
+            var firstPosition = new int[26];
+
+            var offset = 97;
+            for (var i = 0; i < str.Length; i++)
+            {
+                counter[(int)str[i] - offset]++;
+                if(firstPosition[(int)str[i] - offset] < 1)
+                {
+                    firstPosition[(int)str[i] - offset] = i + 1;
+                }
+            };
+
+            var min = str.Length+1;
+            char ch = '[';
+            for (var j = 0; j < firstPosition.Length; j++)
+            {
+                if(counter[j]==1 && firstPosition[j] < min)
+                {
+                    min = firstPosition[j];
+                    ch = str[min-1];
+                }
+            }
+            return ch.ToString();
+        }
+    }
+}
---
+++ StringFilter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace StringFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filter = new Filter();
+            Debug.WriteLine(filter.FindFirstUniqChar("aaattttbbbfzff"));
+        }
+    }
+}
